a41bbbac0e76b4e1c2a1719c67c8c5c6212a93a7
msv-karthik <msvkarthik12@gmail.com>
2026-04-18 20:13:27 +0530

    minor bug fixes-2

--- client/src/socket/socket.js
+++ client/src/socket/socket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const socket = io(import.meta.env.BACKEND_URL,{
+const socket = io(import.meta.env.VITE_BACKEND_URL,{
     transports: ["websocket"],
     autoConnect: true,
     withCredentials: true,
